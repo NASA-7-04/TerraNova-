a2b0d0caf36c5feec5d3f7bdad45b709274d8d4e
Sosumi000 <ggdggn12@gmail.com>
2025-10-04 15:09:56 +0900

    temp build

---
+++ main.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+function init(parent) {
+  const app = express();
+  const corsOptions = {
+    origin: [
+      'http://localhost:8080',
+      'http://192.168.0.230:8080',
+      "https://gengine.co.kr"
+    ],
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
+
+  app.use('', express.static(path.join(__dirname, 'build')));
+
+  app.get('', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+  parent.use("/terranova", app);
+}
+
+module.exports = { init };
--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -43,8 +43,6 @@ const WelcomeWindow = forwardRef(({ }, ref) => {
                 <TextButton1 label={"Kepler-452b"}></TextButton1>
                 <TextButton1 label={"Proxima Centauri b"}></TextButton1>
                 <TextButton1 label={"55 Cancri e"}></TextButton1>
-                <TextButton1 label={"Kepler-69c"}></TextButton1>
-                <TextButton1 label={"Mars"}></TextButton1>
             </DropdownMenu>
 
             <IconButton2 label={"View Details"} icon={"fa-solid fa-paper-plane"}
